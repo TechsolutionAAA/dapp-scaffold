1833e4d2f75430aa3043baac3c26373ffad65d95
Software Engineer <97345885+Gangstar0312@users.noreply.github.com>
2022-07-17 20:30:43 +0200

    init dapp-scaffold project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import { ContextProvider } from 'contexts/ContextProvider';
+import { AppBar } from 'components/AppBar';
+import { ContentContainer } from 'components/ContentContainer';
+import { Footer } from 'components/Footer';
+import Notifications from 'components/Notification'
+
+require('@solana/wallet-adapter-react-ui/styles.css');
+require('styles/globals.css');
+
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+    return (
+        <>
+          <Head>
+            <title>Solana Scaffold Lite</title>
+          </Head>
+
+          <ContextProvider>
+            <div className="flex flex-col h-screen">
+              <Notifications />
+              <AppBar/>
+              <ContentContainer>
+                <Component {...pageProps} />
+              </ContentContainer>
+              <Footer/>
+            </div>
+          </ContextProvider>
+        </>
+    );
+};
+
+export default App;
